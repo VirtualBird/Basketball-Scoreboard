23fa84b0b10f0da80dea15a3b875a7fb407ee5cf
SkyPidgeot <33476798+VirtualBird@users.noreply.github.com>
2023-11-14 21:42:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 function addScore(inputScore, inputElId)
 {
-    //  Grabs element ID from string parameter
     let elemId = document.getElementById(inputElId);
-    //  Grabs content in element id and parses to int value
     let elemValue = parseInt(elemId.textContent);
-    
-    //  Adds score parameter to current value
+
     elemValue += inputScore
     
-    //  Updates value in html document
     elemId.textContent = elemValue;
-    
-    /*
-    console.log(inputScore);
-    console.log(elemId);
-    
-    console.log(elemValue);
-    */
 }
 
 function resetScore(inputElId)
